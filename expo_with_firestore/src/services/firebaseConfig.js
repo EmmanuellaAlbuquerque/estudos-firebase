a36a3578ee066339787e5718e56b032f1d7efc21
Emmanuella Albuquerque <emmanuellaf.albuquerque@gmail.com>
2024-03-19 13:09:59 -0300

    adding firebase config file

---
+++ expo_with_firestore/src/services/firebaseConfig.js
@@ -0,0 +1,25 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { initializeAuth, getReactNativePersistence } from "firebase/auth";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+const firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(AsyncStorage),
+});
+
+auth.useDeviceLanguage();
+
+export { db, auth };
